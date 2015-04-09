d24a22321bea59a7c261ac51cd5db740167ca7ef
john smith <puzzle@gmail.com>
2015-04-09 20:09:16 +0000

    job_board, trying to get show page to work

--- job_board/app/controllers/jobs_controller.rb
+++ job_board/app/controllers/jobs_controller.rb
@@ -1,4 +1,39 @@
 class JobsController < ApplicationController
-  def index
+  def create
+    Job.create(job_params)
+    redirect_to jobs_path
   end
-end
+  
+  def destroy
+    @job = Job.find(params[:id])
+    @job.destroy
+    redirect_to jobs_path
+  end
+  
+  def edit
+    @job = Job.find(params[:id])
+  end
+  
+  def index
+    @jobs = Job.all
+  end
+
+  def new
+    @job = Job.new
+  end
+  
+  def show
+  end
+  
+  def update
+    @job = Job.find(params[:id])
+    @job.update_attributes(job_params)
+    redirect_to jobs_path
+  end
+  
+  private
+  
+  def job_params
+    params.require(:job).permit(:title, :description)
+  end
+end  # class JobsController < ApplicationController
